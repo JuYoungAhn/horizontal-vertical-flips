6bb44dff54d45f8cde50ad74f5c504bbec4ec063
Ju Young Ahn <juyoung.ahn@snu.ac.kr>
2019-07-11 02:26:05 +0900

    eager add

--- .ipynb_checkpoints/augmentation-checkpoint.py
+++ .ipynb_checkpoints/augmentation-checkpoint.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 import tensorflow as tf
+import time
 
 def get_horizontal_vertical_flips(x, ch) : 
     """ get horizontal and vertical flips of images
@@ -15,19 +16,30 @@ def get_horizontal_vertical_flips(x, ch) :
     SAMPLE = x.shape[0]
     WIDTH = x.shape[1]
     HEIGHT = x.shape[2]
-      
+
     augmented_x = np.zeros(shape=[4*SAMPLE,WIDTH,HEIGHT,ch])
-    for i in tqdm(range(0, SAMPLE)):
-        fliped_tensor = tf.image.flip_left_right(x[i])
-        fliped_image = tf.Session().run(fliped_tensor)
-        fliped_tensor2 = tf.image.flip_up_down(x[i])
-        fliped_image2 = tf.Session().run(fliped_tensor2)
-        fliped_tensor3 = tf.image.flip_up_down(fliped_image)
-        fliped_image3 = tf.Session().run(fliped_tensor3)
-        
-        augmented_x[4*i] = x[i] # original image
-        augmented_x[4*i+1] = fliped_image # vertical fliped image
-        augmented_x[4*i+2] = fliped_image2 # horizontal fliped image
-        augmented_x[4*i+3] = fliped_image3 # vertical and horizontal fliped image
-        
+    session = tf.Session()
+    
+    if tf.executing_eagerly() : 
+      print("Eager mode")
+      for i in tqdm(range(0, SAMPLE)):
+          fliped_image = tf.image.flip_left_right(x[i]).numpy()
+          fliped_image2 = tf.image.flip_up_down(x[i]).numpy()
+          fliped_image3 = tf.image.flip_up_down(fliped_image).numpy()
+
+          augmented_x[4*i] = x[i] # original image
+          augmented_x[4*i+1] = fliped_image # vertical fliped image
+          augmented_x[4*i+2] = fliped_image2 # horizontal fliped image
+          augmented_x[4*i+3] = fliped_image3 # vertical and horizontal fliped image
+    else : 
+      for i in tqdm(range(0, SAMPLE)): 
+          fliped_image = session.run(tf.image.flip_left_right(x[i]))
+          fliped_image2 = session.run(tf.image.flip_up_down(x[i]))
+          fliped_image3 = session.run(tf.image.flip_up_down(fliped_image))
+
+          augmented_x[4*i] = x[i]
+          augmented_x[4*i+1] = fliped_image
+          augmented_x[4*i+2] = fliped_image2 
+          augmented_x[4*i+3] = fliped_image3
+
     return augmented_x
